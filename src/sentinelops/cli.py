4a2dbf7615f2cd31509d61685dd43eb3d7c1b803
Glenn Lewis <glelewis90@gmail.com>
2026-05-11 21:03:10 -0500

    Added basic scan

--- src/sentinelops/cli.py
+++ src/sentinelops/cli.py
@@ -1,10 +1,16 @@
 import click
+import subprocess
 from sentinelops import __version__
 
 @click.command("sentinelops")
 @click.version_option(__version__, prog_name="sentinelops")
-def main():
-    click.echo("Welcome to SentinelOps")
+@click.argument("site")
+def main(site):
+    if site is None:
+        click.echo(f"Welcome to SentinelOps {__version__}. Use --help for more information.")
+    else:
+        scan_result = subprocess.run(["curl", "-IL", site], capture_output=True, text=True)
+        click.echo(scan_result.stdout)
 
 if __name__ == "__main__":
     main()
